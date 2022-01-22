1d5fdbb2edbdaa795b0a1a13401d9c3f6f3f67b3
Amisha <amishaadwani0023@gmail.com>
2022-01-22 22:08:38 +0530

    new commit

---
+++ src/components/Upper.js
@@ -0,0 +1,13 @@
+import React from "react"
+import image from "../images/photo-grid.png"
+
+export default function Upper() {
+    return (
+        <section>
+ <img src= {image} className="Upper-logo"/>
+ <h1 className="Upper-head"> Online Experiences </h1>
+ <p className="Upper-text"> Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+</p>
+        </section>
+    )
+}
