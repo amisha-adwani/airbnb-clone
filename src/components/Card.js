4309f36f11ab12a5ef6871118f1df2bfc2fadcae
Amisha <amishaadwani0023@gmail.com>
2022-01-29 21:10:23 +0530

    new commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,16 +3,22 @@ import star from "../images/star.png"
 
 export default function Card(props){
     console.log(props)
+        let badgeText
+        if(props.item.openSpots === 0){
+            badgeText = "SOLD OUT"
+        } else if(props.item.location === "Online"){
+            badgeText ="ONLINE"
+        }
     return(
         <div className="card">
-            {props.openSpots === 0 && <div className="cardBadge"> SOLD OUT </div>}
-            <img src={props.image} className="image" />
+            {badgeText && <div className="cardBadge"> {badgeText} </div>}
+            <img src={props.item.coverImg} className="image" />
             <div className="card-stats">
             <img src={star} className="star" /> 
-            <span className="bold"> {props.rating}  </span> ({props.reviewCount}) USA 
+            <span className="bold"> {props.item.stats.rating}  </span> ({props.item.stats.reviewCount}) USA 
             </div>
-            <p className="card-text"> {props.title} </p>
-            <p className="card-text"> <span className="bold"> From ${props.price}</span>/person</p>
+            <p className="card-text"> {props.item.title} </p>
+            <p className="card-text"> <span className="bold"> From ${props.item.price}</span>/person</p>
 
         </div>
     )
